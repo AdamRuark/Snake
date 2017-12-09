e1f61a86587d008ef3e0b0114350dc75f8607ee8
AdamRuark <adamruark15@gmail.com>
2017-12-08 22:25:49 -0800

    Created psuedotable to track snake body parts

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 window.onload = createBoard;
 window.onkeydown = userInput;
 
+//global values. Only need to exist once
+var intervalId = null;
+var key = null;
+var snake = {row: 15, col:15, len:6};
+var psuedoTable = [];
 
+
+//create html table and pseduotable for snake tracking
 function createBoard(){
 	var tr, td;
 	var main = document.getElementsByTagName("main");
@@ -11,21 +18,17 @@ function createBoard(){
 	//add all the columns and rows
 	for(var i = 0; i < 30; i++){
 		tr = document.createElement("tr");
+		psuedoTable[i] = [];
 		for(var j = 0; j < 30; j++){
 			td = document.createElement("td");
+			td.classList.add("no-snake");
 			tr.appendChild(td);
-
+			psuedoTable[i][j] = 0;
 		}
 		table.appendChild(tr);
 	}
 	main.appendChild(table);
 }
-
-
-//global values. Only need to exist once
-var intervalId = null;
-var key = null;
-var pos = {row: 15, col:15};
 
 function userInput(e){
 
@@ -33,7 +36,7 @@ function userInput(e){
 
 	//call gameLoop once here to initialize it. Start in center
 	if (intervalId == null) {
-    	gameLoop(pos);
+    	gameLoop(snake);
     }
 }
 
@@ -49,58 +52,58 @@ function changeKey(inputKey){
 }
 
 //main game loop. Runs from game start.
-function gameLoop(pos){
-	if(checkCollision(pos)){
+function gameLoop(snake){
+	//if snake runs into something, end game
+	if(checkCollision(snake)){
 		console.log("GAME OVER");
 		clearInterval(intervalId);
 	}
 	else{
 		//change the color
-		changeClass("snake", pos);
+		changeClass("snake", snake);
 
 		//change current position
-		move(pos);
+		move(snake);
 
 		//set it on loop indefinitely
 		if(intervalId){
 			clearInterval(intervalId);
 		}
-		intervalId = setInterval(function(){gameLoop(pos);}, 100);
+		intervalId = setInterval(function(){gameLoop(snake);}, 100);
 	}
 }
 
 //increment or decrement current position based on direction
-function move(pos){
+function move(snake){
 	switch(key){
 		//left
 		case 37:
-			pos.col--;
+			snake.col--;
 			break;
 		//up
 		case 38:
-			pos.row--;
+			snake.row--;
 			break;
 		//right
 		case 39:
-			pos.col++;
+			snake.col++;
 			break;
 		//down
 		case 40:
-			pos.row++;
+			snake.row++;
 			break;
 	}
 }
 
-function checkCollision(pos){
+function checkCollision(snake){
 
 	//check if out of bounds
-	if(pos.row < 0 || pos.row >= 30 || pos.col < 0 || pos.col >= 30){
+	if(snake.row < 0 || snake.row >= 30 || snake.col < 0 || snake.col >= 30){
 		return true;
 	}
 	
 	//check for collision with itself
-	var curNode = document.getElementsByTagName("tr")[pos.row].childNodes[pos.col];
-	console.log(curNode);
+	var curNode = document.getElementsByTagName("tr")[snake.row].childNodes[snake.col];
 	if(curNode.classList.contains("snake")){
 		return true;
 	}
@@ -109,7 +112,7 @@ function checkCollision(pos){
 	return false;
 }
 
-function changeClass(newClass, pos){
+function changeClass(newClass, snake){
 	var tr = document.getElementsByTagName("tr");
-	tr[pos.row].childNodes[pos.col].classList.add(newClass);
+	tr[snake.row].childNodes[snake.col].classList.add(newClass);
 }
