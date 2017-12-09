2590212042ba453a4301c7edd11d8be1db72c896
AdamRuark <adamruark15@gmail.com>
2017-12-09 03:07:41 -0800

    Changed snake to be more OO. Removed psuedoTable

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-window.onload = createBoard;
+window.onload = createGame;
 window.onkeydown = userInput;
 
 //global values. Only need to exist once
 var intervalId = null;
-var key = null;
-var psuedoTable = [];
-var snake = {row: 15, col:15, len:6};
+var key = 40;
+var snake = {body:[], len:6};
 var lockInput = 0;
+var gameStart = 0;
 
-//create html table and pseduotable for snake tracking
-function createBoard(){
+//create html table and snake object
+function createGame(){
 	var tr, td;
 	var main = document.getElementsByTagName("main");
 	var table = document.createElement("table");
 	main = main[0];
 
-	//add all the columns and rows
+	//add all the columns and rows to display board and pseudo table
 	for(var i = 0; i < 30; i++){
 		tr = document.createElement("tr");
-		psuedoTable[i] = [];
 		for(var j = 0; j < 30; j++){
 			td = document.createElement("td");
 			td.classList.add("no-snake");
 			tr.appendChild(td);
-			psuedoTable[i][j] = -1;
 		}
 		table.appendChild(tr);
 	}
 	main.appendChild(table);
+
+	for(var i = 0; i < snake.len; i++){
+		snake.body.push({row:15-i, col:15});
+		changeClass("snake", 15-i, 15);
+	}
 }
 
 function userInput(e){
-	changeKey(e.keyCode);
+	//update direction
+	if(gameStart == 1){
+		changeKey(e.keyCode);
+	}
 
-	//call gameLoop once here to initialize it. Start in center
-	if (intervalId == null) {
-    	gameLoop(snake);
+	// call gameLoop once here to initialize it. Start in center
+	if (intervalId == null && e.keyCode == 32) {
+		gameStart = 1;
+    	gameLoop();
     }
 }
 
 function changeKey(inputKey){
-
-	if(lockInput){
-		console.log("LOCKED");
-	}
 	//modulo math to determine if current key is opposite of input key
 	var temp = (inputKey - 35)%4 + 37;
 
@@ -54,10 +57,6 @@ function changeKey(inputKey){
 		//lock so it can't be changed until the next cycle
 		lockInput = 1;
 	}
-
-
-
-	
 }
 
 //main game loop. Runs from game start.
@@ -65,7 +64,10 @@ function gameLoop(){
 	//unlock input
 	lockInput = 0;
 
-	//move snake, this always occurs
+	//store previous head location in case of move
+	var prevHead = JSON.parse(JSON.stringify(snake.body[0]));
+
+	//move head, this always occurs
 	move();
 
 	//if snake runs into something, end game
@@ -74,11 +76,9 @@ function gameLoop(){
 		clearInterval(intervalId);
 	}
 	else{
-		//change the color
-		changeClass("snake", snake.row, snake.col);
 
-		//update display to reflect move
-		updateTable();
+		//update snake part locations
+		updateSnake(prevHead);
 
 		//set it on loop indefinitely
 		if(intervalId){
@@ -88,21 +88,26 @@ function gameLoop(){
 	}
 }
 
-function updateTable(){
-	//decrement the rest of the body
-	for(var i = 0; i < 30; i++){
-		for(var j = 0; j < 30; j++){
-			if(psuedoTable[i][j] > 0){
-				psuedoTable[i][j]--;
-			}
-			else if(psuedoTable[i][j] == 0){
-				changeClass("no-snake", i, j);
-				psuedoTable[i][j]--;
-			}
-		}
+function updateSnake(prevHead){
+	//remove tail
+	var row = snake.body[snake.len-1].row;
+	var col = snake.body[snake.len-1].col;
+	changeClass("no-snake", row, col);
+
+	//update current head
+	row = snake.body[0].row;
+	col = snake.body[0].col;
+	changeClass("snake", row, col);
+
+	//cycle body part coordinates
+	var prev = prevHead;
+	var next;
+	for(var i = 1; i < snake.len; i++){
+		//store previous value to replace next in body
+		next = snake.body[i];
+		snake.body[i] = prev;
+		prev = next;
 	}
-	//set head to max value
-	psuedoTable[snake.row][snake.col] = snake.len;
 }
 
 //increment or decrement current position based on direction
@@ -110,19 +115,19 @@ function move(){
 	switch(key){
 		//left
 		case 37:
-			snake.col--;
+			snake.body[0].col--;
 			break;
 		//up
 		case 38:
-			snake.row--;
+			snake.body[0].row--;
 			break;
 		//right
 		case 39:
-			snake.col++;
+			snake.body[0].col++;
 			break;
 		//down
 		case 40:
-			snake.row++;
+			snake.body[0].row++;
 			break;
 	}
 }
@@ -130,12 +135,12 @@ function move(){
 function checkCollision(){
 
 	//check if out of bounds
-	if(snake.row < 0 || snake.row >= 30 || snake.col < 0 || snake.col >= 30){
+	if(snake.body[0].row < 0 || snake.body[0].row >= 30 || snake.body[0].col < 0 || snake.body[0].col >= 30){
 		return true;
 	}
 	
 	//check for collision with itself
-	var curNode = document.getElementsByTagName("tr")[snake.row].childNodes[snake.col];
+	var curNode = document.getElementsByTagName("tr")[snake.body[0].row].childNodes[snake.body[0].col];
 	if(curNode.classList.contains("snake")){
 		return true;
 	}
