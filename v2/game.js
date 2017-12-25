5eaf78a7cd1ce160da7209104aa58b2a67615c3a
AdamRuark <adamruark15@gmail.com>
2017-12-25 01:05:07 -0800

    Board now renders

--- v2/game.js
+++ v2/game.js
@@ -6,14 +6,24 @@ window.addEventListener('keydown', function(e){
 
 function startGame() {
 	gameArea.start();
-	snake = new Snake("green", 10, 120);
+	board = new Board(25);
+	snake = new Snake("green", 0, 0, 30);
+}
+
+function updateGameArea() {
+	gameArea.clear();
+	snake.move();
+	board.draw();
+	snake.draw();
 }
 
 var gameArea = {
 	canvas : document.createElement("canvas"),
 	start : function() {
-		this.canvas.width = 500;
-		this.canvas.height = 500;
+		//TODO: Get start input from the user and define game state from that
+
+		this.canvas.width = 700;
+		this.canvas.height = 700;
 		this.context = this.canvas.getContext("2d");
 		this.ignoreMove = false;
 		// this.key = 40 /*start game off moving down*/
@@ -21,7 +31,7 @@ var gameArea = {
 		//add to window and set up main loop
 		var main = document.getElementsByClassName("main-game")[0];
 		main.insertBefore(this.canvas, main.childNodes[0]);
-		this.interval = setInterval(updateGameArea, 20);
+		this.interval = setInterval(updateGameArea, 100);
 	},
 	clear : function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -30,7 +40,6 @@ var gameArea = {
 }
 
 function inputHandler(snake) {
-
 	//if the user presses space, pause or play the game depending on current state
 	if(gameArea.key == 32){
 		if(gameArea.interval){
@@ -39,7 +48,7 @@ function inputHandler(snake) {
 			gameArea.ignoreMove = true;
 		}
 		else {
-			gameArea.interval = setInterval(updateGameArea, 20);
+			gameArea.interval = setInterval(updateGameArea, 100);
 			gameArea.ignoreMove = false;
 		}
 	}
@@ -50,47 +59,57 @@ function inputHandler(snake) {
 	}
 }
 
+function Board(num){
+	this.size = 700;
+	this.num = num;
+	this.width = this.size/this.num;
 
-function Snake(color, x, y) {
-	this.width = 30;
-	this.height = 30;
+	this.draw = function(){
+		ctx = gameArea.context;
+		for (var i = 0; i <= this.size; i += this.width) {
+			ctx.moveTo(i, 0);
+			ctx.lineTo(i, this.size);
+
+			ctx.moveTo(0, i);
+			ctx.lineTo(this.size, i);
+		}
+		ctx.strokeStyle = "black";
+		ctx.stroke();
+	}
+	this.draw();
+}
+
+function Snake(color, x, y, size) {
+	this.size = size;
 	this.x = x;
 	this.y = y;
-	
-	ctx = gameArea.context;
-	ctx.fillStyle = color;
-	ctx.fillRect(this.x, this.y, this.width, this.height);
 
-	this.update = function(){
+	this.draw = function(){
 		ctx = gameArea.context;
 		ctx.fillStyle = color;
-		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.fillRect(this.x, this.y, this.size, this.size);
 	};
+	this.draw();
 
 	this.move = function(){
 		switch(this.direction){
-			case 37: this.x -= 1; break;
-			case 38: this.y -= 1; break;
-			case 39: this.x += 1; break;
-			case 40: this.y += 1; break;
+			case 37: this.x -= this.size; break;
+			case 38: this.y -= this.size; break;
+			case 39: this.x += this.size; break;
+			case 40: this.y += this.size; break;
 		}
 	};
 
-	this.isValidMove = function(key){
-		var temp = (key - 35)%4 + 37;
+	this.isValidMove = function(){
+		var temp = (gameArea.key - 35)%4 + 37;
 		return temp != this.direction && gameArea.ignoreMove == false;
 	}
 
 	this.changeDirection = function(){
 		//if we can changed the direction, change it.
-		if(this.isValidMove(gameArea.key)) {
+		if(this.isValidMove()) {
 			this.direction = gameArea.key;
 		}
 	}
 }
 
-function updateGameArea() {
-	gameArea.clear();
-	snake.move();
-	snake.update();
-}
