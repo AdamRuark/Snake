f1f8ac8a56bcc1591042f20dc52513e99080d156
AdamRuark <adamruark15@gmail.com>
2017-12-24 23:21:43 -0800

    Game input handler in progress.
    Play and pause controlled by space bar. Arrow keys control direction

--- v2/game.js
+++ v2/game.js
@@ -11,6 +11,8 @@ var gameArea = {
 		this.canvas.width = 500;
 		this.canvas.height = 500;
 		this.context = this.canvas.getContext("2d");
+		this.ignoreInput = false;
+		this.key = 40 /*start game off moving down*/
 
 		//add to window and set up main loop
 		var main = document.getElementsByClassName("main-game")[0];
@@ -18,13 +20,33 @@ var gameArea = {
 		this.interval = setInterval(updateGameArea, 20);
 
 		//event listener
-		window.addEventListener('keydown', function (e) {
-			gameArea.key = e.keyCode;
-		})
+		window.addEventListener('keydown', eventHandler);
 	},
 	clear : function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	},
+	inputHandler: function (currentKey) {
+
+		//if the user presses space, pause or play the game depending on current state
+		if(currentKey == 32){
+			console.log(this.interval);
+			if(this.interval){
+				clearInterval(this.interval);
+				this.interval = null;
+				this.ignoreInput = true;
+			}
+			else {
+				this.interval = setInterval(updateGameArea, 20);
+				this.ignoreInput = false;
+			}
+		}
 	}
+}
+
+
+function eventHandler(e){
+	gameArea.key = e.keyCode;
+	gameArea.inputHandler(gameArea.key);
 }
 
 function Snake(color, x, y) {
@@ -33,6 +55,7 @@ function Snake(color, x, y) {
 	this.x = x;
 	this.y = y;
 	this.prevDir = 0;
+	
 	ctx = gameArea.context;
 	ctx.fillStyle = color;
 	ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -44,15 +67,15 @@ function Snake(color, x, y) {
 	};
 
 	this.move = function(){
-		var key = gameArea.key;
 
-		//verify current key is valid input
-		if(key < 37 || key > 40) return;
+		//store current key incase key value changes while processing
+		var key = gameArea.key;
 
 		//if not a valid move, keep going in same direction
 		if(!this.isValidMove(key)) {
 			key = this.prevDir;
 		}
+
 
 		switch(key){
 			case 37: this.x -= 1; break;
@@ -65,7 +88,7 @@ function Snake(color, x, y) {
 
 	this.isValidMove = function (key){
 		var temp = (key - 35)%4 + 37;
-		return temp != this.prevDir;
+		return temp != this.prevDir && gameArea.ignoreInput == false;
 	}
 }
 
