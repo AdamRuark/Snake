3233b660752b03f94847bd15804a49e134de30c9
AdamRuark <adamruark15@gmail.com>
2017-12-28 01:05:39 -0800

    Starting database stuff

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+
+const file = "public/scores.json";
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/addScore/:score/:username', function(req, res, next){
 	console.log(req.params.username + ": " + req.params.score);
+	fs.appendFile(file, req.params.username + ": " + req.params.score + "\n");
+
 });
 
 app.get('/*', function (req, res) {
@@ -17,3 +22,11 @@ app.get('/*', function (req, res) {
 app.listen(port, function(){
 	console.log("Server has started on port: " + port + "\n");
 });
+
+//TODO: Score Json object
+/*	Idea: One json object 'table'
+	Each entry is a place on the table (so an array)
+	Each array entry holds an object
+	Each object contains 2 values: name, score	
+	Sort by score
+*/
