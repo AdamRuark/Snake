f3eafec033ccbc4333c004f603d9e699276ef96e
AdamRuark <adamruark15@gmail.com>
2017-12-27 22:58:37 -0800

    Snake now served properly

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-var http = require('http');
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-	// body...
-})
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.get('/addScore', function(req, res){
+	console.log("Score added");
+});
+
+app.get('/*', function (req, res) {
+	res.render('public/index');
+});
+
+//set up server ports
+app.listen(port, function(){
+	console.log("Server has started on port: " + port + "\n");
+});
