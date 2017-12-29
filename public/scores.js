3b29304410046f6c7e36f88c14a4dea6ff9cd0c4
AdamRuark <adamruark15@gmail.com>
2017-12-28 23:20:46 -0800

    Now with less errors

--- public/scores.js
+++ public/scores.js
@@ -1,14 +1,11 @@
 function getScoreList(){
 	var xhttp = new XMLHttpRequest();
-	var scores;
 
 	xhttp.onreadystatechange = function() { 
-        if (xhttp.readyState == 4 && xhttp.status == 200){
-        	scores = JSON.parse(xhttp.responseText);
-
-        	displayScoreList(scores);
-        }
-    }
+		if (xhttp.readyState == 4 && xhttp.status == 200){
+			displayScoreList(JSON.parse(xhttp.responseText));
+		}
+	}
 	xhttp.open("GET", "getScore", true);
 	xhttp.send();
 }
@@ -16,11 +13,11 @@ function getScoreList(){
 function displayScoreList(scores){
 	var scoresDOM = document.getElementById("score-list");
 	for(var i = 0; i < 5; ++i){
-		var bullet = document.createElement('li');
-		var value = document.createTextNode(scores[i].name + " " + scores[i].score);
-		bullet.appendChild(value);
-		scoresDOM.appendChild(bullet);
+		if(i < scores.length){
+			var bullet = document.createElement('li');
+			var value = document.createTextNode(scores[i].name + " " + scores[i].score);
+			bullet.appendChild(value);
+			scoresDOM.appendChild(bullet);
+		}
 	}
-
-	console.log(scores)
 }
